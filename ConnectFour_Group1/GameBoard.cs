812777a44d9bcc36572d2987ee5eca0b01982d31
dgilliam2 <dgilliam2@my.monroeccc.edu>
2024-04-07 15:14:09 -0400

    moved functions to GameBoard

--- ConnectFour_Group1/GameBoard.cs
+++ ConnectFour_Group1/GameBoard.cs
@@ -15,7 +15,8 @@ namespace Connect4Testing
     {
         //make 2d array with 6 rows and 7 columns
         public static Button[,] boardArray = new Button[6, 7];
-
+        public static int playerTurn = 0;
+        private static Label turnDisplay;
         public static void AddPieces(Panel pnl)
         {
             //clear out array to avoid repetition or breakage
@@ -31,6 +32,63 @@ namespace Connect4Testing
                     columns = 0;
                     rows++;
                 }
+            }
+        }
+
+        public static void SetLabel(Label lbl)
+        {
+            turnDisplay = lbl;
+        }
+        public static void DropPieces(int colindex)
+        {
+            int rowindex = GetRow(colindex);
+
+            if (rowindex != -1)
+            {
+                if (playerTurn == 0)
+                {
+                    turnDisplay.Text = "Player Two's Turn";
+                    boardArray[rowindex, colindex].BackgroundImage = Properties.Resources.YellowPiece2;
+                    playerTurn++;
+                }
+                else
+                {
+                    turnDisplay.Text = "Player One's Turn";
+                    boardArray[rowindex, colindex].BackgroundImage = Properties.Resources.RedPiece2;
+                    playerTurn--;
+                }
+            }
+        }
+
+        public static int GetRow(int colindex)
+        {
+            Button currentButton;
+            for (int row = 0; row <= GameBoard.boardArray.GetLength(0) - 1; row++)
+            {
+                currentButton = GameBoard.boardArray[row, colindex];
+                if (currentButton.BackgroundImage == null)
+                {
+                    return row;
+                }
+            }
+            return -1;
+        }
+        // could use something like this to change the background pics and just use the buttons as display while they aren't enabled
+        public static void Piece_Placement(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            int colIndex = -1;
+            if (clickedButton.Name == "btn_ColumnZero") colIndex = 0;
+            else if (clickedButton.Name == "btn_ColumnOne") colIndex = 1;
+            else if (clickedButton.Name == "btn_ColumnTwo") colIndex = 2;
+            else if (clickedButton.Name == "btn_ColumnThree") colIndex = 3;
+            else if (clickedButton.Name == "btn_ColumnFour") colIndex = 4;
+            else if (clickedButton.Name == "btn_ColumnFive") colIndex = 5;
+            else if (clickedButton.Name == "btn_ColumnSix") colIndex = 6;
+
+            if (colIndex != -1)
+            {
+                DropPieces(colIndex);
             }
         }
     }
--- ConnectFour_Group1/SinglePlayerForm.cs
+++ ConnectFour_Group1/SinglePlayerForm.cs
@@ -13,7 +13,6 @@ namespace Connect4Testing
 {
     public partial class SinglePlayerForm : Form
     {
-        int playerTurn = 0;
         private WelcomeForm wForm;
 
         public SinglePlayerForm(WelcomeForm wf)
@@ -21,6 +20,7 @@ namespace Connect4Testing
             InitializeComponent();
             CenterToScreen();
             GameBoard.AddPieces(pnl_BoardPanel);
+            GameBoard.SetLabel(lbl_TurnDisplay);
             wForm = wf;
             int num = 0;
             foreach (Button btn in GameBoard.boardArray)
@@ -49,58 +49,7 @@ namespace Connect4Testing
             Application.Exit();
         }
 
-        private void DropPieces(int colindex)
-        {
-            int rowindex = GetRow(colindex);
-
-            if (rowindex != -1)
-            {
-                if (playerTurn == 0)
-                {
-                    lbl_TurnDisplay.Text = "Player Two's Turn";
-                    GameBoard.boardArray[rowindex, colindex].BackgroundImage = Properties.Resources.YellowPiece2;
-                    playerTurn++;
-                }
-                else
-                {
-                    lbl_TurnDisplay.Text = "Player One's Turn";
-                    GameBoard.boardArray[rowindex, colindex].BackgroundImage = Properties.Resources.RedPiece2;
-                    playerTurn--;
-                }
-            }
-        }
-        
-        private int GetRow(int colindex)
-        {
-            Button currentButton;
-            for (int row = 0; row <= GameBoard.boardArray.GetLength(0) - 1; row++) 
-            { 
-                currentButton = GameBoard.boardArray[row, colindex];
-                if (currentButton.BackgroundImage == null)
-                {
-                    return row;
-                }
-            }
-            return -1;
-        }
-        // could use something like this to change the background pics and just use the buttons as display while they aren't enabled
-        private void Piece_Placement(object sender, EventArgs e)
-        {
-            Button clickedButton = (Button)sender;
-            int colIndex = -1;
-            if (clickedButton == btn_ColumnZero) colIndex = 0;
-            else if (clickedButton == btn_ColumnOne) colIndex = 1;
-            else if (clickedButton == btn_ColumnTwo) colIndex = 2;
-            else if (clickedButton == btn_ColumnThree) colIndex = 3;
-            else if (clickedButton == btn_ColumnFour) colIndex = 4;
-            else if (clickedButton == btn_ColumnFive) colIndex = 5;
-            else if (clickedButton == btn_ColumnSix) colIndex = 6;
-
-            if (colIndex != -1)
-            {
-                DropPieces(colIndex);
-            }
-        }
+       
         // uses the public function in WelcomeForm to exit the program when the red 'X' is playerTurn
         private void SinglePlayerForm_FormClosed(object sender, FormClosedEventArgs e)
         {
