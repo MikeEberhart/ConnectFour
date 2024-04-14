6d8de2c4d9eca94d689df4fc157a6a6ec6fd7d47
chaseothan <52476148+chaseothan@users.noreply.github.com>
2024-04-14 15:49:51 -0400

    made stuff for the sound
    made stuff for the sound

---
+++ ConnectFour_Group1/Sound.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Connect4Testing
+{
+    public partial class Sound
+    {
+        Stream soundFile;
+        SoundPlayer player;
+    }
+
+    private void btn_applause_Click()
+    {
+        soundFile = Properties.Resources.click.wav; 
+        player = new SoundPlayer(soundFile); 
+        playSound();
+    }
+    private void btn_applause_warn()
+    {
+        soundFileWarn = Properties.Resources.warn.wav;
+        player = new SoundPlayer(soundFile);
+        playSound();
+    }
+    private void playSound()
+    {
+        if (checkBox_continuous.Checked)
+        {
+            player.PlayLooping();
+        }
+        else
+        {
+            player.Play();
+        }
+    }
+}
